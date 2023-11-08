5dab541ad97fd36b02ffb3dae84c5cd9a4471302
lewmas9152 <smnjuguna22@gmail.com>
2023-11-08 08:53:37 +0300

    enabled links

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,21 @@ function App() {
           <div className="content">
             <h3>Njuguna Samwel</h3>
             <p className="proffesion">Frontend Developer</p>
-            <a href="">SamwelNjuguna.website</a>
+            <a href="https://lewmas9152.github.io/portfolio/" target="_blank">
+              SamwelNjuguna.website
+            </a>
             <div className="socials">
               <button className="email">
                 <img src="/images/email.svg" alt="emailIcon" />
                 Email
               </button>
 
-              <button className="linkedin">
-                <img src="/images/linkedin.svg" alt="emailIcon" />
-                LinkedIn
-              </button>
+              <a href="https://www.linkedin.com/in/samwel-njuguna-1797a6240/">
+                <button className="linkedin">
+                  <img src="/images/linkedin.svg" alt="emailIcon" />
+                  LinkedIn
+                </button>
+              </a>
             </div>
 
             <section>
@@ -39,18 +43,39 @@ function App() {
             <section>
               <h4>Interests</h4>
               <p className="interests">
-                Food expert. Music scholar. Reader. Internet fanatic. Bacon
-                buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee
+                Computer Scientist. Reader. Internet fanatic. Bacon buff.
+                Entrepreneur. Travel geek. African culture fanatic. Coffee
                 fanatic.
               </p>
             </section>
           </div>
           <footer>
-            <img src="/images/twitter.svg" alt="twitterIcon"  className="icon"/>
-            <img src="/images/facebook.svg" alt="facebookIcon"  className="icon"/>
-            <img src="/images/instagram.svg" alt="instagramIcon"  className="icon"/>
-            <img src="/images/github.svg" alt="githubIcon"  className="icon"/>
-            
+            <a href="https://twitter.com/lewmas9152" target="_blank">
+              {" "}
+              <img
+                src="/images/twitter.svg"
+                alt="twitterIcon"
+                className="icon"
+              />
+            </a>
+            <a href="https://www.facebook.com/patsam%20njugush" target="_blank">
+              {" "}
+              <img
+                src="/images/facebook.svg"
+                alt="facebookIcon"
+                className="icon"
+              />
+            </a>
+            <a href="https://www.instagram.com/patsam_njugush/" target="_blank">
+              <img
+                src="/images/instagram.svg"
+                alt="instagramIcon"
+                className="icon"
+              />
+            </a>
+            <a href="https://github.com/lewmas9152" target="_blank">
+              <img src="/images/github.svg" alt="githubIcon" className="icon" />
+            </a>
           </footer>
         </div>
       </main>
