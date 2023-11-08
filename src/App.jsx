18b8d7089c73234525ea0b9a48da74f648d4e46b
lewmas9152 <smnjuguna22@gmail.com>
2023-11-08 08:58:54 +0300

    linked images to root

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,13 @@ function App() {
             </a>
             <div className="socials">
               <button className="email">
-                <img src="/images/email.svg" alt="emailIcon" />
+                <img src="./images/email.svg" alt="emailIcon" />
                 Email
               </button>
 
               <a href="https://www.linkedin.com/in/samwel-njuguna-1797a6240/">
                 <button className="linkedin">
-                  <img src="/images/linkedin.svg" alt="emailIcon" />
+                  <img src="./images/linkedin.svg" alt="emailIcon" />
                   LinkedIn
                 </button>
               </a>
@@ -53,7 +53,7 @@ function App() {
             <a href="https://twitter.com/lewmas9152" target="_blank">
               {" "}
               <img
-                src="/images/twitter.svg"
+                src="./images/twitter.svg"
                 alt="twitterIcon"
                 className="icon"
               />
@@ -61,20 +61,20 @@ function App() {
             <a href="https://www.facebook.com/patsam%20njugush" target="_blank">
               {" "}
               <img
-                src="/images/facebook.svg"
+                src="./images/facebook.svg"
                 alt="facebookIcon"
                 className="icon"
               />
             </a>
             <a href="https://www.instagram.com/patsam_njugush/" target="_blank">
               <img
-                src="/images/instagram.svg"
+                src="./images/instagram.svg"
                 alt="instagramIcon"
                 className="icon"
               />
             </a>
             <a href="https://github.com/lewmas9152" target="_blank">
-              <img src="/images/github.svg" alt="githubIcon" className="icon" />
+              <img src="./images/github.svg" alt="githubIcon" className="icon" />
             </a>
           </footer>
         </div>
